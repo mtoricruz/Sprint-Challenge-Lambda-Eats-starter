c5a8a505882c9c8921cafe59c1c4e42c8139c155
toricruzmendiola <mtoricruz@gmail.com>
2020-04-24 08:32:23 -0700

    added dependencies and skeleton structure of mvp code. will work on testing and then solve a bug regarding the checkboxed items not appearing in the DOM

--- ReadMe.md
+++ ReadMe.md
@@ -13,8 +13,16 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your Team Lead.
 
 1. In 1-2 sentences, explain what React's `useRouteMatch` hook is used for.
+
+    useRouteMatch allows us to add nested routes in our app so we can change a single parameter in the url without having to change the whole thing.
+
 2. How would you explain form validation to someone who has never programmed before?
+
+    Form validation is a method we can code into a form to ensure the future user will input the correct information needed in the respective box, and if they don’t the form will automatically tell them what they have to do to make it correct. (ex: this field is required, need 4 characters minimum, etc.
+
 3. In 1-2 sentences, define end to end testing.
+
+    End-to-end testing is a stage in testing that mimics real UI interaction such as submitting forms, typing in fields, scrolling through the app, etc. They help point out bugs and other non-intuitive points that a user would encounter while using the website.
 
 You are expected to be able to answer all these questions. Your responses contribute to your Sprint Challenge grade. Skipping this section **will** prevent you from passing this challenge.
 
@@ -24,14 +32,14 @@ You are expected to be able to answer all these questions. Your responses contri
 
 Follow these steps to set up and work on your project:
 
-- [ ] Create a forked copy of this project.
-- [ ] Add your project manager as collaborator on Github.
-- [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-- [ ] CD into the project base directory `cd lambda-eats`
-- [ ] Download project dependencies by running `npm install`
-- [ ] Start up the app using `npm start`
-- [ ] Create a new branch: git checkout -b `<firstName-lastName>`. Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-- [ ] Push commits: git push origin `<firstName-lastName>`.
+- [x] Create a forked copy of this project.
+- [x] Add your project manager as collaborator on Github.
+- [x] Clone your OWN version of the repository (Not Lambda's by mistake!).
+- [x] CD into the project base directory `cd lambda-eats`
+- [x] Download project dependencies by running `npm install`
+- [x] Start up the app using `npm start`
+- [x] Create a new branch: git checkout -b `<firstName-lastName>`. Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+- [x] Push commits: git push origin `<firstName-lastName>`.
 
 ### Task 2: Minimum Viable Product
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,152 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// STEP 2 - import Route/Switch/Link from react router dom if needed
+import { Route, Link } from "react-router-dom";
+import PizzaForm from "./PizzaForm";
+import Order from "./Order"
+// import HomePage from './HomePage';
+import axios from "axios";
+import * as yup from "yup";
+import "./App.css";
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+const url = "https://reqres.in/api/users";
+
+const initialFormValues = {
+  //text for name
+  name: "",
+  //dropdown comp for size
+  size: "",
+  //checkbox for toppings
+  toppings: {
+    sausage: false,
+    pepperoni: false,
+    peppers: false,
+    apples: false
+  },
+  //text for instructions
+  instructions: ""
 };
+
+const initialFormErrors = {
+  name: "",
+  size: "",
+  instructions: ""
+};
+
+const formSchema = yup.object().shape({
+  name: yup.string().min(2, "name must have at least 2 characters!"),
+  size: yup.string().matches(/(small|medium)/, "either small or medium size"),
+  instructions: yup.string()
+});
+
+function App() {
+  const [orders, setOrders] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formDisabled, setFormDisabled] = useState(true);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const postOrder = order => {
+    axios.post(url, order)
+      .then(res => {
+        setOrders([...orders, res.data]);
+      })
+      .catch(err => {
+        debugger
+      });
+  };
+
+  useEffect(() => {
+    formSchema.isValid(formValues).then(valid => {
+      setFormDisabled(!valid);
+    });
+  }, [formValues]);
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+
+    const newOrder = {
+      name: formValues.name,
+      size: formValues.size === 'medium' ? false : true,
+      toppings: Object.keys(formValues.toppings)
+      .filter(topping => formValues.toppings[topping] === true),
+      instructions: formValues.instructions
+    };
+
+    postOrder(newOrder);
+    setFormValues(initialFormValues);
+  };
+
+  const onInputChange = evt => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+
+    yup.reach(formSchema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ""
+        });
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        });
+      });
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
+
+  const onCheckboxChange = evt => {
+    const { name } = evt.target;
+    const isChecked = evt.target.isChecked;
+
+    setFormValues({
+      ...formValues,
+      toppings: {
+        ...setFormValues.toppings,
+        [name]: isChecked
+      }
+    });
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Lambda Eats</h1>
+      </header>
+
+      
+      <Route exact path="/">
+        <Link to="/pizza">Order Here</Link>
+        <h1>Home Page</h1>
+      </Route>
+
+      <Route path="/pizza">
+        <Link to="/">Go Home</Link>
+        <h2>Order Form</h2>
+        <PizzaForm
+          values={formValues}
+          onInputChange={onInputChange}
+          onCheckboxChange={onCheckboxChange}
+          onSubmit={onSubmit}
+          disabled={formDisabled}
+          errors={formErrors}
+        />
+      </Route>
+
+      {
+        orders.map(order => {
+          return (
+            <Order key={order.id} details={order} />
+          )
+        })
+      }
+    </div>
+  );
+}
+
 export default App;
